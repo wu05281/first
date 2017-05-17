0a8011258d5d348b53e7fd2295a625dcc27d8350
wu05281 <37809131@qq.com>
2017-05-17 18:30:07 +0800

    excel读取,改进range右侧不包含写法bug修复

--- excel_op.py
+++ excel_op.py
@@ -8,7 +8,12 @@ print('此sheet有%s行,%s列数据' % (sheet.max_row, sheet.max_column))
 #         # cell有value，row、column 和 coordinate 属性
 #         print(cell_object.coordinate, cell_object.value)
 #         print('--- END OF ROW ---')
-for i in range(1, sheet.max_row):
-    for j in range(1, sheet.max_column):
+
+#range(1,5) #代表从1到5(不包含5);  range(5) #代表从0到5(不包含5)
+for i in range(1, sheet.max_row+1):
+    for j in range(1, sheet.max_column+1):
         print(sheet.cell(row=i, column=j).value)
     print('--- END OF ROW ---')
+
+# for cell_obj in sheet.columns[1]:
+#     print(cell_obj.value)
