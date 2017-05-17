cd4d7f5a84f28e2cdf7fea7f314d20a6ff91f8a7
wu05281 <37809131@qq.com>
2017-05-17 18:10:00 +0800

    excel读取,改进

--- excel_op.py
+++ excel_op.py
@@ -3,8 +3,12 @@ import openpyxl
 wb = openpyxl.load_workbook('example.xlsx')
 sheet = wb.get_active_sheet()
 print('此sheet有%s行,%s列数据' % (sheet.max_row, sheet.max_column))
-for row_cell_objects in sheet['A1':'C1']:
-    for cell_object in row_cell_objects:
-        # cell有value，row、column 和 coordinate 属性
-        print(cell_object.coordinate, cell_object.value)
-        print('--- END OF ROW ---')
+# for row_cell_objects in sheet['A1':'C1']:
+#     for cell_object in row_cell_objects:
+#         # cell有value，row、column 和 coordinate 属性
+#         print(cell_object.coordinate, cell_object.value)
+#         print('--- END OF ROW ---')
+for i in range(1, sheet.max_row):
+    for j in range(1, sheet.max_column):
+        print(sheet.cell(row=i, column=j).value)
+    print('--- END OF ROW ---')
